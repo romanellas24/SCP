c9fd8ec43474541114e2d10ec5385d8de8da48b5
daniele.romanella02@gmail.com <www.daniele.romanella@gmail.com>
2025-02-02 19:34:47 +0100

    Avanzamento

--- src/main/scala/First.scala
+++ src/main/scala/First.scala
@@ -6,15 +6,45 @@ object First extends App {
   println("Spark is running!")
   //private val df = spark.read.format("csv").option("header", "true").load("/home/romanellas/Documents/order_products.csv/order_products.csv")
   //private val df = spark.read.format("csv").option("header", "true").load("/home/romanellas/Documents/order_products.csv/small.csv")
-  //val rdd = spark.read.csv("/home/romanellas/Documents/order_products.csv/small.csv").rdd
-  val rdd = spark.read.csv("/home/romanellas/Documents/order_products.csv/medium.csv").rdd
-  val pairs = rdd.map(e => {
+  private val rdd = spark.read.csv("/home/romanellas/Documents/order_products.csv/small.csv").rdd
+  //val rdd = spark.read.csv("/home/romanellas/Documents/order_products.csv/medium.csv").rdd
+
+  //Given an RDD[String] We'll parse all as (O, P) Where, O is the order and P is the product
+  private val orderProductPair = rdd.map(e => {
     val tmp = e.toString().replace("[", "").replace("]", "").split(",")
     (tmp.head.toInt, tmp.tail.head.toInt)
   })
-  val parsed = pairs.groupBy(e => {
+
+  //Now we can group by order to get a collection of pairs (O, P)
+  private val orderAllProduct = orderProductPair.groupBy(e => {
     e._1
   })
-  println(parsed.collect().take(0).toList)
+
+  //Now we can get an RDD[List] Where every list is a set of products P in the same order. We do not need order ID anymore.
+  private val onlyCompactBuffers = orderAllProduct.map(e => {
+    e._2
+  })
+  //Now we can delete first key from allCompactBuffers and we'll get only RDDs with product ID.
+  private val onlyProducts = onlyCompactBuffers.map(e => {
+    e.map(pair => {
+      pair._2
+    })
+  })
+
+  //Now, for every pair Product ID RDD, we have to get all pairs.
+  private val allProductPairs = onlyProducts.flatMap(productIds => {
+    for {
+      x <- productIds
+      y <- productIds
+      if x < y
+    } yield (x, y)
+  })
+
+  private val result = allProductPairs.groupBy(e => e).map(e => {
+    val (x, y) = e._1
+    (x, y, e._2.size)
+  }).
+
+  println(result.collect().toList)
 
 }
