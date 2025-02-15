27a0dfa383c8a8499aa96067bbcca12cb8f70297
romanellas <www.daniele.romanella@gmail.com>
2025-02-15 13:04:24 +0100

    AV

--- src/main/scala/project/Fourth.scala
+++ src/main/scala/project/Fourth.scala
@@ -1,8 +1,7 @@
 package project
 
 import org.apache.spark.sql.SparkSession
-import project.Second.{clock, partitioned}
-import project.utils.{Clock, OrderPartitioner}
+import project.utils.{Clock, OrderPartitioner, ProductsPartitioner}
 
 
 object Fourth extends App {
@@ -17,9 +16,9 @@ object Fourth extends App {
   println("Spark is running!")
   private val filename = args.apply(0)
   //private val rdd = spark.read.csv("./data/order_products.csv").rdd
-  private val rdd = spark.read.csv("./data/medium.csv").rdd
+  //private val rdd = spark.read.csv("./data/medium.csv").rdd
   //private val rdd = spark.read.csv("./data/small.csv").rdd
-  //private val rdd = spark.read.csv("gs://order-dataset/data/" + filename).rdd
+  private val rdd = spark.read.csv("gs://order-dataset/data/" + filename).rdd
 
   //Given an RDD[String] We'll parse all as (O, P) Where, O is the order and P is the product
   private val orderProductPair = rdd.map(e => {
@@ -27,26 +26,26 @@ object Fourth extends App {
     (tmp.head.toInt, tmp.tail.head.toInt)
   })
 
-  private val partitions = 100
+  private val partitions = 256
   private val partitioner = new OrderPartitioner(partitions)
   private val partitioned = orderProductPair.partitionBy(partitioner)
   //val test = partitioned.groupBy(e => e._1)
-  val test = partitioned.groupByKey().mapValues(iterable => iterable)
+  val test = partitioned.groupByKey()
     .flatMapValues(productIds =>
-    for {
-      x <- productIds
-      y <- productIds
-      if x < y
-    } yield (x, y)
-  ).map(iterable => iterable._2)
+      for {
+        x <- productIds
+        y <- productIds
+        if x < y
+      } yield (x, y)
+    ).map(iterable => (iterable._2._1, iterable._2._2))
     .groupBy(e => e).map(e => {
-    val (x, y) = e._1
-    (x, y, e._2.size)
-  })
+      val (x, y) = e._1
+      (x, y, e._2.size)
+    })
 
-  println(test.collect().toList)
-  //val df = spark.createDataFrame(test).repartition(1)
-  //df.write.format("csv").option("path", "gs://order-dataset/out/out-fourth-" + filename).save()
+  //test.collect().toList
+  val df = spark.createDataFrame(test).repartition(1)
+  df.write.format("csv").option("path", "gs://order-dataset/out/out-fourth-" + filename).save()
   //df.write.format("csv").option("path", "out.csv").save()
   clock.printElapsedTime()
 }
